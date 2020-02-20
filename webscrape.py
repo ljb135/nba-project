c07fc9e715a7dfe03e0e905a817ddb6b85c8ec80
Chris Liang <christopherliang.cl@gmail.com>
2020-02-20 12:26:49 -0500

    Functioning Request For Game IDs

--- webscrape.py
+++ webscrape.py
@@ -1,16 +1,19 @@
 import urllib.request
+import gzip
 
-day = "12%2F07%2F2001"
-gameid_url = f"https://stats.nba.com/stats/scoreboardV2?DayOffset=0&LeagueID=00&gameDate={day}"
-gameid_headers = {"Host": "stats.nba.com", "Accept": "application/json, text/plain, */*", "x-nba-stats-token": "true"}
-gameid = "0020100265"
-gamestats_url = f"https://stats.nba.com/stats/boxscoretraditionalv2?EndPeriod=10&EndRange=28800&GameID={gameid}&RangeType=0&StartPeriod=1&StartRange=0"
 
-req = urllib.request.Request(url=gameid_url, headers=gameid_headers)
-print(gameid_url)
-try:
-    urllib.request.urlopen(req)
-except urllib.error.URLError as e:
-    print(e.reason)
-# the_page = response.read()
-# print(the_page)
+def games_on_date(month, day, year):
+    day = f"{month}%2F{day}%2F{year}"
+    gameid_url = f"https://stats.nba.com/stats/scoreboardV2?DayOffset=0&LeagueID=00&gameDate={day}"
+    gameid_headers = {"Host": "stats.nba.com", "Connection": "keep-alive", "Accept": "application/json, text/plain, */*", "x-nba-stats-origin": "stats", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36", "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en;q=0.9"}
+
+    req = urllib.request.Request(url=gameid_url, headers=gameid_headers, method="GET")
+    response = urllib.request.urlopen(req)
+    data = response.read()
+    data = str(gzip.decompress(data),'utf-8')
+    return data
+
+print(games_on_date(11,14,2003))
+
+# gameid = "0020100265"
+# gamestats_url = f"https://stats.nba.com/stats/boxscoretraditionalv2?EndPeriod=10&EndRange=28800&GameID={gameid}&RangeType=0&StartPeriod=1&StartRange=0"
