19db0f60bfc78822f139b07f1a21c839d04d30d6
Chris Liang <christopherliang.cl@gmail.com>
2020-02-25 13:57:19 -0500

    New Method to Get Game IDs from the JSON

--- webscrape.py
+++ webscrape.py
@@ -1,5 +1,6 @@
 import urllib.request
 import gzip
+import json
 
 
 def games_on_date(month, day, year):
@@ -11,20 +12,32 @@ def games_on_date(month, day, year):
     response = urllib.request.urlopen(req)
     data = response.read()
     data = str(gzip.decompress(data), 'utf-8')
-    return data
+    json_file = json.loads(data)
+    return json_file
 
 
 def stats_in_game(game_id):
     gamestats_url = f"https://stats.nba.com/stats/boxscoretraditionalv2?EndPeriod=10&EndRange=28800&GameID={game_id}&RangeType=0&StartPeriod=1&StartRange=0"
-    gamestats_headers = {"Host": "stats.nba.com", "Connection": "keep-alive", "Accept": "application/json, text/plain, */*", "x-nba-stats-origin": "stats", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36", "Referer": "https://stats.nba.com/game/0021900751/", "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en;q=0.9"}
+    gamestats_headers = {"Host": "stats.nba.com", "Connection": "keep-alive", "Accept": "application/json, text/plain, */*", "x-nba-stats-origin": "stats", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36", "Referer": f"https://stats.nba.com/game/{game_id}/", "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en;q=0.9"}
 
     req = urllib.request.Request(url=gamestats_url, headers=gamestats_headers)
     response = urllib.request.urlopen(req)
     data = response.read()
     data = str(gzip.decompress(data), 'utf-8')
-    return data
+    json_file = json.loads(data)
+    return json_file
 
 
-print(games_on_date(11, 29, 2003))
-print(stats_in_game("0020100265"))
+def get_game_ids(json_file):
+    game_list = json_file["resultSets"][0]["rowSet"]
+    game_ids = []
+    for game in game_list:
+        game_ids.append(game[2])
+    return game_ids
+
+
+for game_id in get_game_ids(games_on_date("11", "01", "2001")):
+    print(stats_in_game(game_id))
+# print(json.dumps(games_on_date("12", "07", "2002"), indent=4))
+# print(json.dumps(stats_in_game("0020200282"), indent=4))
 
