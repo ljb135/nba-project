73e1886629468db4d690e12028a71b0f50679cac
Chris Liang <christopherliang.cl@gmail.com>
2020-02-28 21:18:45 -0500

    Changed Game class to NBAGame and Finished Initialization

--- webscrape.py
+++ webscrape.py
@@ -3,31 +3,64 @@ import gzip
 import json
 
 
-class game:
-    gameid = None
-    hometeam_id = None
-    awayteam_id = None
-    hometeam_points = None
-    awayteam_points = None
-    hometeam_players = None
-    awayteam_players = None
-    result = None
+class NBAGame:
+    def __init__(self, game_id, json_file):
+        self.game_id = game_id
+        self.json_file = json_file
+        self.home_team_id = None
+        self.away_team_id = None
+        self.home_team_points = None
+        self.away_team_points = None
+        self.home_team_players = []
+        self.away_team_players = []
+        self.home_win = None
 
-    def __init__(self, gameid, hometeam_id, awayteam_id):
-        self.gameid = gameid
-        self.hometeam_id = hometeam_id
-        self.awayteam_id = awayteam_id
+        self.__set_team_ids()
+        self.__set_points()
+        self.__set_player_stats()
+        self.__set_result()
 
-    def get_points(self):
+    def __set_team_ids(self):
+        for i in range(len(self.json_file["resultSets"][2]["rowSet"])):
+            if self.json_file["resultSets"][2]["rowSet"][i][0] == self.game_id:
+                self.home_team_id = self.json_file["resultSets"][2]["rowSet"][i][1]
+                self.away_team_id = self.json_file["resultSets"][2]["rowSet"][i][2]
+                return
 
+    def __set_points(self):
+        for i in range(len(self.json_file["resultSets"][1]["rowSet"])):
+            if self.json_file["resultSets"][1]["rowSet"][i][3] == self.home_team_id:
+                self.home_team_points = self.json_file["resultSets"][1]["rowSet"][i][22]
+            if self.json_file["resultSets"][1]["rowSet"][i][3] == self.away_team_id:
+                self.away_team_points = self.json_file["resultSets"][1]["rowSet"][i][22]
+            if self.home_team_points is not None and self.away_team_points is not None:
+                return
+
+    def __set_player_stats(self):
+        self.json_file = stats_in_game(self.game_id)
+        for player in self.json_file["resultSets"][0]["rowSet"]:
+            if player[1] == self.home_team_id:
+                self.home_team_players.append(player)
+            else:
+                self.away_team_players.append(player)
+
+    def __set_result(self):
+        if self.home_team_points > self.away_team_points:
+            self.home_win = True
+        else:
+            self.home_win = False
+
+    def print(self):
+        attrs = vars(self)
+        print('\n'.join("%s: %s" % item for item in attrs.items()))
 
 
 def games_on_date(month, day, year):
     day = f"{month}%2F{day}%2F{year}"
-    gameid_url = f"https://stats.nba.com/stats/scoreboardV2?DayOffset=0&LeagueID=00&gameDate={day}"
-    gameid_headers = {"Host": "stats.nba.com", "Connection": "keep-alive", "Accept": "application/json, text/plain, */*", "x-nba-stats-origin": "stats", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36", "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en;q=0.9"}
+    game_id_url = f"https://stats.nba.com/stats/scoreboardV2?DayOffset=0&LeagueID=00&gameDate={day}"
+    game_id_headers = {"Host": "stats.nba.com", "Connection": "keep-alive", "Accept": "application/json, text/plain, */*", "x-nba-stats-origin": "stats", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36", "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en;q=0.9"}
 
-    req = urllib.request.Request(url=gameid_url, headers=gameid_headers)
+    req = urllib.request.Request(url=game_id_url, headers=game_id_headers)
     response = urllib.request.urlopen(req)
     data = response.read()
     data = str(gzip.decompress(data), 'utf-8')
@@ -36,10 +69,10 @@ def games_on_date(month, day, year):
 
 
 def stats_in_game(game_id):
-    gamestats_url = f"https://stats.nba.com/stats/boxscoretraditionalv2?EndPeriod=10&EndRange=28800&GameID={game_id}&RangeType=0&StartPeriod=1&StartRange=0"
-    gamestats_headers = {"Host": "stats.nba.com", "Connection": "keep-alive", "Accept": "application/json, text/plain, */*", "x-nba-stats-origin": "stats", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36", "Referer": f"https://stats.nba.com/game/{game_id}/", "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en;q=0.9"}
+    game_stats_url = f"https://stats.nba.com/stats/boxscoretraditionalv2?EndPeriod=10&EndRange=28800&GameID={game_id}&RangeType=0&StartPeriod=1&StartRange=0"
+    game_stats_headers = {"Host": "stats.nba.com", "Connection": "keep-alive", "Accept": "application/json, text/plain, */*", "x-nba-stats-origin": "stats", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36", "Referer": f"https://stats.nba.com/game/{game_id}/", "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en;q=0.9"}
 
-    req = urllib.request.Request(url=gamestats_url, headers=gamestats_headers)
+    req = urllib.request.Request(url=game_stats_url, headers=game_stats_headers)
     response = urllib.request.urlopen(req)
     data = response.read()
     data = str(gzip.decompress(data), 'utf-8')
@@ -55,10 +88,12 @@ def get_game_ids(json_file):
     return game_ids
 
 
-# games = games_on_date("11", "01", "2001")
+games = games_on_date("11", "01", "2019")
+NBAGame(game_id=get_game_ids(games)[0], json_file=games).print()
+
 # for game_id in get_game_ids(games):
 #     stats = stats_in_game(game_id)
 #     print(json.dumps(stats, indent=4))
 
 # print(json.dumps(games_on_date("11", "01", "2001"), indent=4))
-print(json.dumps(stats_in_game("0020100019"), indent=4))
+# print(json.dumps(stats_in_game("0020100019"), indent=4))
