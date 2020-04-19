0b4f95a6f8da4e2b6b34e3da3122e90314c00779
Chris Liang <christopherliang.cl@gmail.com>
2020-04-19 16:52:06 -0400

    Custom architecture in new method

--- neuralnet.py
+++ neuralnet.py
@@ -1,9 +1,7 @@
 from numpy import *
 from pandas import *
-from keras.models import Sequential
-from keras.models import load_model
-from keras.layers import Dense
-from keras.layers import Dropout
+from keras.models import Model, Sequential, load_model
+from keras.layers import Dense, Input, Concatenate, Lambda, Dropout
 from keras.constraints import maxnorm
 import csv
 import matplotlib.pyplot as plt
@@ -45,10 +43,10 @@ def analyze_train(train_csv_filename, test_csv_filename):
     y_test = test_dataset[:, 1]
 
     model = Sequential()
-    model.add(Dense(256, input_dim=546, activation='relu', kernel_constraint=maxnorm(3)))
-    model.add(Dropout(0.25))
-    # model.add(Dense(64, activation='relu', kernel_constraint=maxnorm(3)))
-    # model.add(Dropout(0.1))
+    model.add(Dense(32, input_dim=546, activation='relu', kernel_constraint=maxnorm(3)))
+    model.add(Dropout(0.1))
+    model.add(Dense(8, activation='relu', kernel_constraint=maxnorm(3)))
+    model.add(Dropout(0.1))
     model.add(Dense(1, activation='sigmoid'))
 
     # model.add(Dense(64, input_dim=546, activation='relu'))
@@ -57,7 +55,54 @@ def analyze_train(train_csv_filename, test_csv_filename):
 
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-    history = model.fit(x_train, y_train, epochs=50, batch_size=128, validation_data=(x_test, y_test), verbose=1)
+    history = model.fit(x_train, y_train, epochs=75, batch_size=128, validation_data=(x_test, y_test), verbose=1)
+    print(model.evaluate(x_test, y_test))
+
+    # graph training vs. validation accuracy over epochs
+    plt.figure(1)
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+
+    # graph training vs. validation loss over epochs
+    plt.figure(2)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+    return model
+
+
+def analyze_train_v2(train_csv_filename, test_csv_filename):
+    train_dataset = loadtxt(train_csv_filename, delimiter=',')
+    test_dataset = loadtxt(test_csv_filename, delimiter=',')
+
+    # split into input (X) and output (Y) variables
+    x_train = train_dataset[:, 2:]
+    y_train = train_dataset[:, 1]
+
+    x_test = test_dataset[:, 2:]
+    y_test = test_dataset[:, 1]
+
+    inputTensor = Input((546,))
+    group = []
+    for id in range(0, 21):
+        group.append(Lambda(lambda x: x[:, id:546:21], output_shape=((26,)))(inputTensor))
+        group[id] = Dropout(0.1)(Dense(1, activation='relu', kernel_constraint=maxnorm(3))(group[id]))
+    outputTensor = Concatenate()(group)
+    outputTensor = Dense(1, activation='sigmoid')(Dense(4, activation='relu', kernel_constraint=maxnorm(3))(outputTensor))
+    model = Model(inputTensor, outputTensor)
+
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    history = model.fit(x_train, y_train, epochs=75, batch_size=128, validation_data=(x_test, y_test), verbose=1)
     print(model.evaluate(x_test, y_test))
 
     # graph training vs. validation accuracy over epochs
