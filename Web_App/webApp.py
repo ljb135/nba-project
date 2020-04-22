c21f71c29b1921e805b83a26e3e5145c35a60d81
Chris Liang <christopherliang.cl@gmail.com>
2020-04-21 20:32:13 -0400

    Creation of Form

--- Web_App/webApp.py
+++ Web_App/webApp.py
@@ -1,11 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, url_for, render_template, redirect
+from selectionForm import selectionForm
+
+app = Flask(__name__, instance_relative_config=False)
+app.config.from_object('config.Config')
 
 
-@app.route("/")
-
-def hello():
-    return "<h1>Not Much Going On Here</h1>"
-
+@app.route('/', methods=('GET', 'POST'))
+def contact():
+    form = selectionForm()
+    if form.validate_on_submit():
+        return redirect(url_for('success'))
+    return render_template('index.html', form=form)
 
 app.run(host='0.0.0.0', port=50000)
