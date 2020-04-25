6efaa38dd65044ca9de3ab5cb063ed53dad5884d
Anshul Mittal <anshulmittal07@gmail.com>
2020-04-25 18:20:48 -0400

    Code Organization --> added new HTML files

--- Web_App/webApp.py
+++ Web_App/webApp.py
@@ -1,20 +1,31 @@
 from flask import Flask, url_for, render_template, redirect
-import forms
+from forms import PlayerSelectionForm
 import os
 
 template_dir = os.path.abspath('../../NBA-Project/Web_App/templates')
 print(template_dir)
 
 app = Flask(__name__, template_folder=template_dir)
-app.config['SECRET_KEY'] = 'any secret string'
+app.config['SECRET_KEY'] = 'ao19s2en1638nsh6msh172kd0s72ksj2'
 
 
 @app.route('/', methods=('GET', 'POST'))
-def contact():  # RENAME
-    form = forms.SelectionForm()
+def homepage():  # RENAME
+    form = PlayerSelectionForm()
     if form.validate_on_submit():
         return redirect(url_for('success'))
-    return render_template('request.html', form=form)
+    return render_template('request.html', form=form, title='Home')
 
 
-app.run(host='0.0.0.0', port=50000, debug=True)
+@app.route('/prediction')
+def prediction():
+    return render_template('result.html', title='Prediction')
+
+
+@app.route('/playerlist')
+def playerlist():
+    return render_template('playerlist.html', title='Player List')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=50000, debug=True)
